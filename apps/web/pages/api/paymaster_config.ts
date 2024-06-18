008c6fd3975b89d59bb1ef77d829d68273823455
keshav <keshav17@berkeley.edu>
2024-06-18 15:03:44 -0700

    Paymaster Component

---
+++ apps/web/pages/api/paymaster_config.ts
@@ -0,0 +1,26 @@
+import { baseSepolia } from "viem/chains";
+import { ENTRYPOINT_ADDRESS_V06 } from "permissionless";
+import { createClient, createPublicClient, http } from "viem";
+import { paymasterActionsEip7677 } from "permissionless/experimental";
+import { pimlicoPaymasterActions } from 'permissionless/actions/pimlico';
+import { createPimlicoPaymasterClient } from 'permissionless/clients/pimlico'
+
+
+
+export const client = createPublicClient({
+    chain: baseSepolia,
+    transport: http(),
+  });
+
+const paymasterService = process.env.PAYMASTER_SERVICE_URL!;
+
+export const paymasterClient = createClient({
+    chain: baseSepolia,
+    transport: http(paymasterService),
+  }).extend(paymasterActionsEip7677({ entryPoint: ENTRYPOINT_ADDRESS_V06 }));
+
+// const cloudPaymaster = createPimlicoPaymasterClient({
+//     chain: baseSepolia,
+//     transport: http(rpcUrl),
+//     entryPoint: ENTRYPOINT_ADDRESS_V06,
+//   });
---
+++ apps/web/src/components/Paymaster/Paymaster.tsx
@@ -0,0 +1,52 @@
+import { useMemo, useState } from 'react';
+import { useAccount } from 'wagmi';
+import { useCapabilities, useWriteContracts } from 'wagmi/experimental';
+import {RegistrarControllerABI} from "../../constants"
+import { Abi } from 'viem';
+
+
+export function Paymaster() {
+    const account = useAccount();
+    const[id, setId] = useState<string | undefined>(undefined);
+    const { writeContracts } = useWriteContracts();
+    const {data: availableCapacities} = useCapabilities({
+        account: account.address
+    });
+    const capabilities = useMemo(() => {
+        if (!account.isConnected || !account.chainId || !availableCapacities) {
+            return {};
+        }
+        const chainCapabilities = availableCapacities[account.chainId];
+        if (chainCapabilities["paymasterService"] && chainCapabilities["paymasterService"].supported) {
+            return {
+                paymasterService: {
+                    url: `${document.location.origin}/api/paymaster`
+                },
+            };
+        }
+        return {};
+    }, [availableCapacities, account.chainId]);
+    return (
+        <div>
+            <h2>Mint your basename!</h2>
+            <button 
+            onClick={() => {
+                writeContracts({
+                    contracts: [
+                        {
+                            address: "0x22", // change this
+                            abi: RegistrarControllerABI, // fix error
+                            functionName: "register",
+                            args: [] // get args
+                        }
+                    ], capabilities
+                });
+                
+            }}>
+                Mint
+                </button>
+        </div>
+
+    )
+
+}
